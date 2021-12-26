d08be4d186f29bf9d88944c5414e3a90b7278375
Lzhdontinvolute <2390545586@qq.com>
2021-12-26 14:58:47 +0800

    ch3

---
+++ ch3/KEY_volatile/MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'KEY_volatile' 
+ * Target:  'KEY_volatile' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
